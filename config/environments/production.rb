20e027e6e81e75072f6b71096856dac611e4d5f6
Baochuan Lu <lubaochuan@gmail.com>
2015-06-18 00:00:42 +0000

    Configure sendgrid in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['lubaochuan@gmail.com'],
-    :password       => ENV['optucourse1'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
