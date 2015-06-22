8cce6cd47ec2361898722b9c4c32f89613845bab
Baochuan Lu <lubaochuan@gmail.com>
2015-06-22 16:31:54 +0000

    Fix host in default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'my-rails-tutorial.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'blu-rails-tutorial.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
     :user_name      => ENV['GMAIL_USERNAME'],
