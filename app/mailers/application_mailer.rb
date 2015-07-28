edca7b7cac9ea5e5d205a3ff3b4895758f8bbb19
Baochuan Lu <lubaochuan@gmail.com>
2015-07-28 21:52:36 +0000

    Add user microposts

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "lubaochuan@gmail.com"
+  default from: "from@example.com"
   layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,10 @@ Rails.application.routes.draw do
   post    'login'   =>  'sessions#create'
   delete  'logout'  =>  'sessions#destroy'
   resources :users
-  resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :account_activations,   only: [:edit]
+  resources :password_resets,       only: [:new, :create, :edit, :update]
   resources :charges
+  resources :microposts,            only: [:create, :destroy]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
